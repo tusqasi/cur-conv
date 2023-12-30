bbb2648330a552903e65f2d07a8a2dd83b906133
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-12-31 03:13:26 +0530

    UI Improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,128 +59,126 @@ const Home: React.FC<{ conversion_table: any }> = ({ conversion_table }) => {
 
 	return <>
 
-		<div className="  flex flex-col   w-screen h-screen items-center   justify-center  p-4 triangle bg-white ">
-			<div className="  ">
-				<div className={`logo-container ${isVisible ? 'slide-in' : ''}`}>
-					<span className={cn(" text-5xl p-1   ", bubblegum_sans.className)} > Currency Converter</span>
-				</div>
-				<div className="  p-10 rounded-lg shadow-md  border-2 border-zinc-50  flex flex-col items-start   bg-white ">
-					<div className="  flex flex-row  item-center   ">
-						<div className="  flex flex-row items-end      ">
-							<div className="  flex flex-col  gap-4    ">
-								<div>
-									<Input
-										type=" text"
-										value={fromValue == 0 ? "" : fromValue.toString()}
-										className="  border-2 border-slate-200 rounded-lg p-2 "
-										onChange={(e) => {
-											// debugger;
-											const regex = new RegExp('[0-9.]*', 'm');
-											const whitespace = new RegExp('/^ *$/mg', 'gm');
-											if (whitespace.exec(e.target.value) !== null || e.target.value == "") {
-												console.log("`` in 'from' value");
-												setFromValue(0);
+		<div className="  flex flex-col   w-screen h-screen items-center   justify-center  p-4  bg-white ">
+			<div className={`logo-container ${isVisible ? 'slide-in' : ''}   transition-transform scale-100 hover:scale-110   `}>
+				<span className={cn(" text-5xl p-1   ", bubblegum_sans.className)} > Currency Converter</span>
+			</div>
+			<div className="  p-10 rounded-lg shadow-md  border-2  border-slate-100  flex flex-col items-start   bg-white ">
+				<div className="  flex flex-row  item-center   ">
+					<div className="  flex flex-row items-end      ">
+						<div className="  flex flex-col  gap-4  w-32  lg:w-60 ">
+							<div>
+								<Input
+									type=" text"
+									value={fromValue == 0 ? "" : fromValue.toString()}
+									className="  border border-slate-200 rounded-lg p-2  text-lg font-bold "  
+									onChange={(e) => {
+										// debugger;
+										const regex = new RegExp('[0-9.]*', 'm');
+										const whitespace = new RegExp('/^ *$/mg', 'gm');
+										if (whitespace.exec(e.target.value) !== null || e.target.value == "") {
+											console.log("`` in 'from' value");
+											setFromValue(0);
+											setToValue(0);
+										}
+										else if (regex.exec(e.target.value) !== null) {
+											const from = e.target.value;
+											setFromValue(parseFloat(parseFloat(from).toFixed(2)));
+											const to_value = parseFloat((parseFloat(from)
+												/ conversion_table[fromCurrency] * conversion_table[toCurrency]).toFixed(2));
+											if (to_value.toString() != "NaN") {
+												setToValue(to_value);
+											}
+											else {
 												setToValue(0);
 											}
-											else if (regex.exec(e.target.value) !== null) {
-												const from = e.target.value;
-												setFromValue(parseFloat(parseFloat(from).toFixed(2)));
-												const to_value = parseFloat((parseFloat(from)
-													/ conversion_table[fromCurrency] * conversion_table[toCurrency]).toFixed(2));
-												if (to_value.toString() != "NaN") {
-													setToValue(to_value);
-												}
-												else {
-													setToValue(0);
-												}
-											}
-											else {
-												console.log("NaN in 'from' value");
-											}
-											// debugger;
-										}} />
-								</div>
-								<div className="  w-full  ">
-									<Select onValueChange={(x) => {
-										setFromCurrency(x);
-										setToValue(parseFloat((fromValue / conversion_table[x] * conversion_table[toCurrency]).toFixed(2)));
-									}}
-										value={fromCurrency}
-									>
-										<SelectTrigger className="   p-1 bg-zinc-100  border-2 border-zinc-200 rounded-lg   ">
-											<SelectValue placeholder=" Currency" />
-										</SelectTrigger>
-										<SelectContent className="  bg-white h-40 w-full ">
-											{
-												Object.entries(countries).map(([code, name], _idx) =>
-													<SelectItem key={code} value={code}>{code}
-														<span className="hidden lg:contents "> - {name}
-														</span></SelectItem>
-												)}
-										</SelectContent >
-									</Select>
-								</div>
-
+										}
+										else {
+											console.log("NaN in 'from' value");
+										}
+										// debugger;
+									}} />
 							</div>
-							<div className="  pl-4 pr-4  ">
-								<button onClick={() => {
-									setToCurrency(fromCurrency);
-									setFromCurrency(toCurrency);
-									setToValue(parseFloat(fromValue.toFixed(2)));
-									setFromValue(parseFloat(toValue.toFixed(2)));
-									// __AUTO_GENERATED_PRINTF_START__
-									console.log("Home#(anon) 1"); // __AUTO_GENERATED_PRINTF_END__
-
-								}}>
-									<LoopIcon />
-								</button>
-							</div>
-							<div className="  flex flex-col  gap-4    ">
-								<div>
-									<Input
-										type=" text"
-										value={toValue == 0 ? "" : toValue.toFixed(2).toString()}
-										className="  border-2 border-slate-200 rounded-lg p-2 "
-										onChange={(e) => {
-											if (!e.target.value.match(/^[0-9]*([.,][0-9]+)?$/)) {
-												// setFromValue(parseFloat(e.target.value));
-												// setToValue(fromValue / conversion_table.rates[fromCurrency] * conversion_table.rates[toCurrency]);
-											}
-											else {
-												console.log("NaN in 'to' value");
-											}
-										}} />
-								</div>
-								<div className="  w-full  ">
-									<Select onValueChange={(x) => {
-										setToCurrency(x);
-										setToValue(fromValue / conversion_table[fromCurrency] * conversion_table[x]);
-									}}
-										value={toCurrency}
-										autoComplete="true"
-									>
-										<SelectTrigger className="  w-full p-1 bg-zinc-100  border-2 border-zinc-200 rounded-lg   ">
-											<SelectValue placeholder=" Currency" className=" items-end" />
-										</SelectTrigger>
-										<SelectContent className="  bg-white h-40 w-full rounded-lg   ">
-											{
-												Object.entries(countries).map(([code, name]) =>
-													<SelectItem key={code} value={code}>
-														{code}
-														<span className="hidden lg:contents "> {name}</span> </SelectItem>
-												)}
-										</SelectContent>
-									</Select>
-								</div>
+							<div className="  w-full  ">
+								<Select onValueChange={(x) => {
+									setFromCurrency(x);
+									setToValue(parseFloat((fromValue / conversion_table[x] * conversion_table[toCurrency]).toFixed(2)));
+								}}
+									value={fromCurrency}
+								>
+									<SelectTrigger className="   p-1 bg-zinc-100  border border-zinc-200 rounded-lg   ">
+										<SelectValue placeholder=" Currency" />
+									</SelectTrigger>
+									<SelectContent className="  bg-white h-40 w-full ">
+										{
+											Object.entries(countries).map(([code, name], _idx) =>
+												<SelectItem key={code} value={code}>{code}
+													<span className="hidden lg:contents "> —  {name}
+													</span></SelectItem>
+											)}
+									</SelectContent >
+								</Select>
 							</div>
 
 						</div>
+						<div className="  pl-4 pr-4  ">
+							<button onClick={() => {
+								setToCurrency(fromCurrency);
+								setFromCurrency(toCurrency);
+								setToValue(parseFloat(fromValue.toFixed(2)));
+								setFromValue(parseFloat(toValue.toFixed(2)));
+								// __AUTO_GENERATED_PRINTF_START__
+								console.log("Home#(anon) 1"); // __AUTO_GENERATED_PRINTF_END__
+
+							}}>
+								<LoopIcon />
+							</button>
+						</div>
+						<div className="  flex flex-col  gap-4 w-32  lg:w-60  ">
+							<div>
+								<Input
+									type=" text"
+									value={toValue == 0 ? "" : toValue.toFixed(2).toString()}
+									className="  border border-slate-200 rounded-lg p-2 text-lg font-bold "
+									onChange={(e) => {
+										if (!e.target.value.match(/^[0-9]*([.,][0-9]+)?$/)) {
+											// setFromValue(parseFloat(e.target.value));
+											// setToValue(fromValue / conversion_table.rates[fromCurrency] * conversion_table.rates[toCurrency]);
+										}
+										else {
+											console.log("NaN in 'to' value");
+										}
+									}} />
+							</div>
+							<div className="  w-full  ">
+								<Select onValueChange={(x) => {
+									setToCurrency(x);
+									setToValue(fromValue / conversion_table[fromCurrency] * conversion_table[x]);
+								}}
+									value={toCurrency}
+									autoComplete="true"
+								>
+									<SelectTrigger className="  w-full p-1 bg-zinc-100  border border-zinc-200 rounded-lg   ">
+										<SelectValue placeholder=" Currency" className=" items-end" />
+									</SelectTrigger>
+									<SelectContent className="  bg-white h-40 w-full rounded-lg   ">
+										{
+											Object.entries(countries).map(([code, name]) =>
+												<SelectItem key={code} value={code}>
+													{code}
+													<span className="hidden lg:contents "> — {name}</span> </SelectItem>
+											)}
+									</SelectContent>
+								</Select>
+							</div>
+						</div>
+
 					</div>
-					<span className=" text-gray-500  text-sm  pt-4 " >
-						1 {fromCurrency} ≈ {(conversion_table[toCurrency] / conversion_table[fromCurrency]).toFixed(2).toString()} {toCurrency}
-					</span>
 				</div>
-			</div >
+				<span className=" text-gray-500  text-sm  pt-4 font-mono " >
+					1 {fromCurrency} ≈ {(conversion_table[toCurrency] / conversion_table[fromCurrency]).toFixed(2).toString()} {toCurrency}
+				</span>
+			</div>
 		</div >
 	</>;
 }
