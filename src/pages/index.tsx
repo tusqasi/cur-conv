c64a3d233a3e3594e284e2f15101202b5be48edd
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-12-30 20:35:29 +0530

    Linted and done+1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,11 @@ export async function getServerSideProps() {
 };
 
 async function getData() {
-	let data = await (await fetch("https://api.frankfurter.app/latest?base=usd", { next: { revalidate: Number.POSITIVE_INFINITY } })).json();
-	return data.rates
+
+
+	let data = { "AUD": 0, "BGN": 0, "BRL": 0, "CAD": 0, "CHF": 0, "CNY": 0, "CZK": 0, "DKK": 0, "EUR": 0, "GBP": 0, "HKD": 0, "HUF": 0, "IDR": 0, "ILS": 0, "INR": 0, "ISK": 0, "JPY": 0, "KRW": 0, "MXN": 0, "MYR": 0, "NOK": 0, "NZD": 0, "PHP": 0, "PLN": 0, "RON": 0, "SEK": 0, "SGD": 0, "THB": 0, "TRY": 0, "ZAR": 0 } ;
+	data = (await (await fetch("https://api.frankfurter.app/latest?base=usd", { next: { revalidate: Number.POSITIVE_INFINITY } })).json()).rates;
+	return data;
 }
 
 
